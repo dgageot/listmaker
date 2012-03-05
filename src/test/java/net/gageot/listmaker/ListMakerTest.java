ac241f5064febb91b819c5811e1f8242db806b7d
David Gageot <david@gageot.net>
2012-03-05 09:34:08 +0100

    LICENSE

--- src/test/java/net/gageot/listmaker/ListMakerTest.java
+++ src/test/java/net/gageot/listmaker/ListMakerTest.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of ListMaker.
+ *
+ * Copyright (C) 2012
+ * "David Gageot" <david@gageot.net>,
+ *
+ * ListMaker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ListMaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ListMaker.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.gageot.listmaker;
 
 import com.google.common.base.*;
